f4ee5a3ce0084f5c5c23980e3ab52abf5390f45d
shailesh074 <shaileshhiremath074@gmail.com>
2026-05-06 11:29:26 +0530

    Added evaluation files

---
+++ experience_AUB.py
@@ -0,0 +1,15 @@
+My experience at Amity University Bengaluru has been highly enriching and growth-oriented.
+
+
+The university provides a professional learning environment with supportive faculty and modern infrastructure.
+
+
+I gained exposure to technical learning, collaborative projects, and innovative thinking opportunities.
+
+
+The campus culture encouraged confidence, communication skills, and overall personality development.
+
+
+My journey at the university continues to strengthen both my academic foundation and career vision.
+
+
