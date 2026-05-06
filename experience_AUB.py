e80ec7a252148d867411be36eaaad079fe15f53e
Shailesh Hiremath <shaileshhiremath074@gmail.com>
2026-05-06 11:36:49 +0530

    Update experience_AUB.py

--- experience_AUB.py
+++ experience_AUB.py
@@ -1,15 +1,9 @@
-print(My experience at Amity University Bengaluru has been highly enriching and growth-oriented.
-
+print("My experience at Amity University Bengaluru has been highly enriching and growth-oriented.
 
 The university provides a professional learning environment with supportive faculty and modern infrastructure.
 
-
 I gained exposure to technical learning, collaborative projects, and innovative thinking opportunities.
-
 
 The campus culture encouraged confidence, communication skills, and overall personality development.
 
-
-My journey at the university continues to strengthen both my academic foundation and career vision.)
-
-
+My journey at the university continues to strengthen both my academic foundation and career vision.")
